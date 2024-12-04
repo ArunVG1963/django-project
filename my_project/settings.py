4078095b6d432131c53d3ff64ace066b878cf945
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-12-04 21:32:06 +0000

    DEBUG set to True in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f8sic-elv-y@qd=psyibuw3qua%6vsszka63#!s2u&ob-#^lo+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-arunvg1963-djangoprojec-z97ws8l5ecz.ws.codeinstitute-ide.net','.herokuapp.com']
 
