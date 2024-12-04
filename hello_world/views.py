7f38484d963f5e412b764ce4f93bc4c421e70932
Arun VenuGopal <ewo73h24@students.codeinstitute.net>
2024-12-04 16:16:54 +0000

    added about view and code, and changed my_project index view to test for request.method as POST or GET

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        return HttpResponse("You Must have POSTEed something")
+    else:
+        return HttpResponse(request.method)
+
     return HttpResponse("Hello, World!")
-    
